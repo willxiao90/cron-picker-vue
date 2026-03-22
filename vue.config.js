ffb6ea5b9f9c0412f0cc5b679145557134a622b3
willxiao90 <xiaoshuoit@163.com>
2026-03-22 16:24:41 +0800

    chore: 打包优化

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,17 @@
 module.exports = {
-    publicPath: '/cron-picker-vue/',
-    pages: {
-      index: {
-        entry: 'example/main.js',
-      },
+  publicPath: "/cron-picker-vue/",
+  pages: {
+    index: {
+      entry: "example/main.js",
     },
-    chainWebpack: config => {
-      config.externals({
-        vue: 'Vue',
-        'element-ui': 'ELEMENT'
-      })
-    }
-  }
+  },
+  productionSourceMap: false,
+  chainWebpack: (config) => {
+    config.externals({
+      vue: "Vue",
+      "element-ui": "ELEMENT",
+    });
+    config.optimization.delete("splitChunks");
+    config.optimization.delete("runtimeChunk");
+  },
+};
